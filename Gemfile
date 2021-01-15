e8a668677b8e3e016e59dbeff86bed1205b5ddf8
Emily Ritter <ritter@gcgmarketing.com>
2021-01-14 19:42:15 -0600

    Replaces site with simple 'under construction' site

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'jekyll'
-gem 'github-pages'
-gem 'markdown', '~> 1.2'
-gem 'kramdown', '~> 1.9'
-gem 'jekyll-paginate', '~> 1.1'
---
+++ main/assets/js/scripts.js
@@ -0,0 +1,504 @@
+// Utility function
+function Util () {};
+
+/* 
+	class manipulation functions
+*/
+Util.hasClass = function(el, className) {
+	if (el.classList) return el.classList.contains(className);
+	else return !!el.getAttribute('class').match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+};
+
+Util.addClass = function(el, className) {
+	var classList = className.split(' ');
+ 	if (el.classList) el.classList.add(classList[0]);
+  else if (!Util.hasClass(el, classList[0])) el.setAttribute('class', el.getAttribute('class') +  " " + classList[0]);
+ 	if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+};
+
+Util.removeClass = function(el, className) {
+	var classList = className.split(' ');
+	if (el.classList) el.classList.remove(classList[0]);	
+	else if(Util.hasClass(el, classList[0])) {
+		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+    el.setAttribute('class', el.getAttribute('class').replace(reg, ' '));
+	}
+	if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+};
+
+Util.toggleClass = function(el, className, bool) {
+	if(bool) Util.addClass(el, className);
+	else Util.removeClass(el, className);
+};
+
+Util.setAttributes = function(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+};
+
+/* 
+  DOM manipulation
+*/
+Util.getChildrenByClassName = function(el, className) {
+  var children = el.children,
+    childrenByClass = [];
+  for (var i = 0; i < el.children.length; i++) {
+    if (Util.hasClass(el.children[i], className)) childrenByClass.push(el.children[i]);
+  }
+  return childrenByClass;
+};
+
+Util.is = function(elem, selector) {
+  if(selector.nodeType){
+    return elem === selector;
+  }
+
+  var qa = (typeof(selector) === 'string' ? document.querySelectorAll(selector) : selector),
+    length = qa.length,
+    returnArr = [];
+
+  while(length--){
+    if(qa[length] === elem){
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/* 
+	Animate height of an element
+*/
+Util.setHeight = function(start, to, element, duration, cb, timeFunction) {
+	var change = to - start,
+	    currentTime = null;
+
+  var animateHeight = function(timestamp){  
+    if (!currentTime) currentTime = timestamp;         
+    var progress = timestamp - currentTime;
+    if(progress > duration) progress = duration;
+    var val = parseInt((progress/duration)*change + start);
+    if(timeFunction) {
+      val = Math[timeFunction](progress, start, to - start, duration);
+    }
+    element.style.height = val+"px";
+    if(progress < duration) {
+        window.requestAnimationFrame(animateHeight);
+    } else {
+    	if(cb) cb();
+    }
+  };
+  
+  //set the height of the element before starting animation -> fix bug on Safari
+  element.style.height = start+"px";
+  window.requestAnimationFrame(animateHeight);
+};
+
+/* 
+	Smooth Scroll
+*/
+
+Util.scrollTo = function(final, duration, cb, scrollEl) {
+  var element = scrollEl || window;
+  var start = element.scrollTop || document.documentElement.scrollTop,
+    currentTime = null;
+
+  if(!scrollEl) start = window.scrollY || document.documentElement.scrollTop;
+      
+  var animateScroll = function(timestamp){
+  	if (!currentTime) currentTime = timestamp;        
+    var progress = timestamp - currentTime;
+    if(progress > duration) progress = duration;
+    var val = Math.easeInOutQuad(progress, start, final-start, duration);
+    element.scrollTo(0, val);
+    if(progress < duration) {
+      window.requestAnimationFrame(animateScroll);
+    } else {
+      cb && cb();
+    }
+  };
+
+  window.requestAnimationFrame(animateScroll);
+};
+
+/* 
+  Focus utility classes
+*/
+
+//Move focus to an element
+Util.moveFocus = function (element) {
+  if( !element ) element = document.getElementsByTagName("body")[0];
+  element.focus();
+  if (document.activeElement !== element) {
+    element.setAttribute('tabindex','-1');
+    element.focus();
+  }
+};
+
+/* 
+  Misc
+*/
+
+Util.getIndexInArray = function(array, el) {
+  return Array.prototype.indexOf.call(array, el);
+};
+
+Util.cssSupports = function(property, value) {
+  if('CSS' in window) {
+    return CSS.supports(property, value);
+  } else {
+    var jsProperty = property.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase();});
+    return jsProperty in document.body.style;
+  }
+};
+
+// merge a set of user options into plugin defaults
+// https://gomakethings.com/vanilla-javascript-version-of-jquery-extend/
+Util.extend = function() {
+  // Variables
+  var extended = {};
+  var deep = false;
+  var i = 0;
+  var length = arguments.length;
+
+  // Check if a deep merge
+  if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+    deep = arguments[0];
+    i++;
+  }
+
+  // Merge the object into the extended object
+  var merge = function (obj) {
+    for ( var prop in obj ) {
+      if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+        // If deep merge and property is an object, merge properties
+        if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+          extended[prop] = extend( true, extended[prop], obj[prop] );
+        } else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  };
+
+  // Loop through each object and conduct a merge
+  for ( ; i < length; i++ ) {
+    var obj = arguments[i];
+    merge(obj);
+  }
+
+  return extended;
+};
+
+// Check if Reduced Motion is enabled
+Util.osHasReducedMotion = function() {
+  if(!window.matchMedia) return false;
+  var matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)');
+  if(matchMediaObj) return matchMediaObj.matches;
+  return false; // return false if not supported
+}; 
+
+/* 
+	Polyfills
+*/
+//Closest() method
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function(s) {
+		var el = this;
+		if (!document.documentElement.contains(el)) return null;
+		do {
+			if (el.matches(s)) return el;
+			el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType === 1); 
+		return null;
+	};
+}
+
+//Custom Event() constructor
+if ( typeof window.CustomEvent !== "function" ) {
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+}
+
+/* 
+	Animation curves
+*/
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+Math.easeInQuart = function (t, b, c, d) {
+	t /= d;
+	return c*t*t*t*t + b;
+};
+
+Math.easeOutQuart = function (t, b, c, d) { 
+  t /= d;
+	t--;
+	return -c * (t*t*t*t - 1) + b;
+};
+
+Math.easeInOutQuart = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t*t + b;
+	t -= 2;
+	return -c/2 * (t*t*t*t - 2) + b;
+};
+
+Math.easeOutElastic = function (t, b, c, d) {
+  var s=1.70158;var p=d*0.7;var a=c;
+  if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+  if (a < Math.abs(c)) { a=c; var s=p/4; }
+  else var s = p/(2*Math.PI) * Math.asin (c/a);
+  return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+};
+
+
+/* JS Utility Classes */
+
+// make focus ring visible only for keyboard navigation (i.e., tab key) 
+(function() {
+  var focusTab = document.getElementsByClassName('js-tab-focus'),
+    shouldInit = false,
+    outlineStyle = false,
+    eventDetected = false;
+
+  function detectClick() {
+    if(focusTab.length > 0) {
+      resetFocusStyle(false);
+      window.addEventListener('keydown', detectTab);
+    }
+    window.removeEventListener('mousedown', detectClick);
+    outlineStyle = false;
+    eventDetected = true;
+  };
+
+  function detectTab(event) {
+    if(event.keyCode !== 9) return;
+    resetFocusStyle(true);
+    window.removeEventListener('keydown', detectTab);
+    window.addEventListener('mousedown', detectClick);
+    outlineStyle = true;
+  };
+
+  function resetFocusStyle(bool) {
+    var outlineStyle = bool ? '' : 'none';
+    for(var i = 0; i < focusTab.length; i++) {
+      focusTab[i].style.setProperty('outline', outlineStyle);
+    }
+  };
+
+  function initFocusTabs() {
+    if(shouldInit) {
+      if(eventDetected) resetFocusStyle(outlineStyle);
+      return;
+    }
+    shouldInit = focusTab.length > 0;
+    window.addEventListener('mousedown', detectClick);
+  };
+
+  initFocusTabs();
+  window.addEventListener('initFocusTabs', initFocusTabs);
+}());
+
+function resetFocusTabsStyle() {
+  window.dispatchEvent(new CustomEvent('initFocusTabs'));
+};
+// File#: _1_header
+// Usage: codyhouse.co/license
+(function() {
+  var mainHeader = document.getElementsByClassName('js-header');
+  if( mainHeader.length > 0 ) {
+    var trigger = mainHeader[0].getElementsByClassName('js-header__trigger')[0],
+      nav = mainHeader[0].getElementsByClassName('js-header__nav')[0];
+
+    // we'll use these to store the node that needs to receive focus when the mobile menu is closed
+    var focusMenu = false;
+
+    //detect click on nav trigger
+    trigger.addEventListener("click", function(event) {
+      event.preventDefault();
+      toggleNavigation(!Util.hasClass(nav, 'header__nav--is-visible'));
+    });
+
+    // listen for key events
+    window.addEventListener('keyup', function(event){
+      // listen for esc key
+      if( (event.keyCode && event.keyCode == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
+        // close navigation on mobile if open
+        if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger)) {
+          focusMenu = trigger; // move focus to menu trigger when menu is close
+          trigger.click();
+        }
+      }
+      // listen for tab key
+      if( (event.keyCode && event.keyCode == 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
+        // close navigation on mobile if open when nav loses focus
+        if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) && !document.activeElement.closest('.js-header')) trigger.click();
+      }
+    });
+
+    // listen for resize
+    var resizingId = false;
+    window.addEventListener('resize', function() {
+      clearTimeout(resizingId);
+      resizingId = setTimeout(doneResizing, 500);
+    });
+
+    function doneResizing() {
+      if( !isVisible(trigger) && Util.hasClass(mainHeader[0], 'header--expanded')) toggleNavigation(false);
+    };
+  }
+
+  function isVisible(element) {
+    return (element.offsetWidth || element.offsetHeight || element.getClientRects().length);
+  };
+
+  function toggleNavigation(bool) { // toggle navigation visibility on small device
+    Util.toggleClass(nav, 'header__nav--is-visible', bool);
+    Util.toggleClass(mainHeader[0], 'header--expanded', bool);
+    trigger.setAttribute('aria-expanded', bool);
+    if(bool) { //opening menu -> move focus to first element inside nav
+      nav.querySelectorAll('[href], input:not([disabled]), button:not([disabled])')[0].focus();
+    } else if(focusMenu) {
+      focusMenu.focus();
+      focusMenu = false;
+    }
+  };
+}());
+
+// File#: _1_revealing-section
+// Usage: codyhouse.co/license
+(function() {
+  var RevealingSection = function(element) {
+    this.element = element;
+    this.scrollingFn = false;
+    this.scrolling = false;
+    this.resetOpacity = false;
+    initRevealingSection(this);
+  };
+
+  function initRevealingSection(element) {
+    // set position of sticky element
+    setBottom(element);
+    // create a new node - to be inserted before the sticky element
+    createPrevElement(element);
+    // on resize -> reset element bottom position
+    element.element.addEventListener('update-reveal-section', function(){
+      setBottom(element);
+      setPrevElementTop(element);
+    });
+    animateRevealingSection.bind(element)(); // set initial status
+    // change opacity of layer
+    var observer = new IntersectionObserver(revealingSectionCallback.bind(element));
+    observer.observe(element.prevElement);
+  };
+
+  function createPrevElement(element) {
+    var newElement = document.createElement("div");
+    newElement.setAttribute('aria-hidden', 'true');
+    element.element.parentElement.insertBefore(newElement, element.element);
+    element.prevElement =  element.element.previousElementSibling;
+    element.prevElement.style.opacity = '0';
+    setPrevElementTop(element);
+  };
+
+  function setPrevElementTop(element) {
+    element.prevElementTop = element.prevElement.getBoundingClientRect().top + window.scrollY;
+  };
+
+  function revealingSectionCallback(entries, observer) {
+    if(entries[0].isIntersecting) {
+      if(this.scrollingFn) return; // listener for scroll event already added
+      revealingSectionInitEvent(this);
+    } else {
+      if(!this.scrollingFn) return; // listener for scroll event already removed
+      window.removeEventListener('scroll', this.scrollingFn);
+      updateOpacityValue(this, 0);
+      this.scrollingFn = false;
+    }
+  };
+
+  function revealingSectionInitEvent(element) {
+    element.scrollingFn = revealingSectionScrolling.bind(element);
+    window.addEventListener('scroll', element.scrollingFn);
+  };
+
+  function revealingSectionScrolling() {
+    if(this.scrolling) return;
+    this.scrolling = true;
+    window.requestAnimationFrame(animateRevealingSection.bind(this));
+  };
+
+  function animateRevealingSection() {
+    if(this.prevElementTop - window.scrollY < window.innerHeight) {
+      var opacity = (1 - (window.innerHeight + window.scrollY - this.prevElementTop)/window.innerHeight).toFixed(2);
+      if(opacity > 0 ) {
+        this.resetOpacity = false;
+        updateOpacityValue(this, opacity);
+      } else if(!this.resetOpacity) {
+        this.resetOpacity = true;
+        updateOpacityValue(this, 0);
+      }
+    }
+    this.scrolling = false;
+  };
+
+  function updateOpacityValue(element, value) {
+    element.element.style.setProperty('--reavealing-section-overlay-opacity', value);
+  };
+
+  function setBottom(element) {
+    var translateValue = window.innerHeight - element.element.offsetHeight;
+    if(translateValue > 0) translateValue = 0;
+    element.element.style.bottom = ''+translateValue+'px';
+  };
+
+  //initialize the Revealing Section objects
+  var revealingSection = document.getElementsByClassName('js-revealing-section');
+  var stickySupported = Util.cssSupports('position', 'sticky') || Util.cssSupports('position', '-webkit-sticky');
+  if( revealingSection.length > 0 && stickySupported) {
+    var revealingSectionArray = [];
+    for( var i = 0; i < revealingSection.length; i++) {
+      (function(i){revealingSectionArray.push(new RevealingSection(revealingSection[i]));})(i);
+    }
+
+    var resizingId = false,
+      customEvent = new CustomEvent('update-reveal-section');
+
+    window.addEventListener('resize', function() {
+      clearTimeout(resizingId);
+      resizingId = setTimeout(doneResizing, 100);
+    });
+
+    // wait for font to be loaded
+    if(document.fonts) {
+      document.fonts.onloadingdone = function (fontFaceSetEvent) {
+        doneResizing();
+      };
+    }
+
+    function doneResizing() {
+      for( var i = 0; i < revealingSectionArray.length; i++) {
+        (function(i){revealingSectionArray[i].element.dispatchEvent(customEvent)})(i);
+      };
+    };
+  }
+}());
